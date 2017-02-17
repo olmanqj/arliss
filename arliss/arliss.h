61fd3c16b9c89a61c24de97c3cd28241f3fdc5ae
olmanqj <olmanqj@hotmail.com>
2017-02-17 01:28:02 -0600

    Rover modes of operation

--- arliss/arliss.h
+++ arliss/arliss.h
@@ -22,3 +22,30 @@ typedef enum {
     closing_up,
     end_navigation
 } ROVER_STATE;
+
+
+
+//////////////////////////////////////////////////
+//  GLOBAL VARIABLES
+/////////////////////////////////////////////////
+
+
+
+
+//////////////////////////////////////////////////
+//  FUNCTIONS
+/////////////////////////////////////////////////
+
+// Definition of all state routines
+void *pre_launch_routine();
+void *ascent_routine();
+void *descent_routine();
+void *navigation_routine();
+void *closing_up_routine();
+void *end_navigation_routine();
+
+// Array of all routines for the different rover_states
+void* (*rover_state_routines[])() = {pre_launch_routine, ascent_routine, descent_routine, navigation_routine, closing_up_routine, end_navigation_routine}; 
+
+
+
--- arliss/arliss.ino
+++ arliss/arliss.ino
@@ -26,9 +26,7 @@
 /////////////////////////////////////////////////
 //  DEFINES
 ////////////////////////////////////////////////
-//#define DEBUG
-
-
+#define DEBUG
 
 
 
@@ -41,21 +39,30 @@ ROVER_STATE current_rover_state;     // Rover state: 0 = groud pre launch, 1 = l
 
 float press, temp;
 
+
+
 //////////////////////////////////////////////////
 //  FUNCTIONS
 /////////////////////////////////////////////////
+
+
 void setup()
 {
   Wire.begin();
   Serial.begin(115200);
   delay(1000);
   
-  Serial.println("\nSetting Barometer");
-  setup_barometer();
-  Serial.println("\nBarometer Ready!!");
+  #ifdef DEBUG
+    Serial.println("\nSetting Barometer");
+  #endif
   
-  Serial.print("\nGround altitude: ");
-  Serial.println(ground_altitude);
+  setup_barometer();
+  
+  #ifdef DEBUG
+    Serial.println("\nBarometer Ready!!");
+    Serial.print("\nGround altitude: ");
+    Serial.println(ground_altitude);
+  #endif
   
   current_rover_state = pre_launch;
 
@@ -66,8 +73,6 @@ void print_barometer_data()
 {
    ////////////////////////Barometro//////////////////////////
   //Temp
-  
-  
   Serial.print(" temp: ");
   float temperature = get_temperature();
   if(temperature) {
@@ -101,19 +106,68 @@ void loop()
 {
   //Serial.print("Current rover state:");
   //Serial.println(current_rover_state);
-  print_barometer_data();
   
+  //print_barometer_data();
   
-  //float temp_alt = get_altitude() ;
-  //Serial.print("Ground Altitude: ");
-  //Serial.print(ground_altitude );
-  //Serial.print(", Absolute Altitude: ");
-  //Serial.print(temp_alt);
-  //Serial.print(", Relative Altitude: ");
-  //Serial.println(ground_altitude - temp_alt );
+  // Execute current rover state corresponding routine
+  (*rover_state_routines[current_rover_state])(); 
+  
   
 }
 
+
+// During the pre launch: wait until launch detection
+void *pre_launch_routine()
+{
+  Serial.println("Rove_status: pre_launch");
+  
+  
+  //current_rover_state = ascent;
+}
+
+void *ascent_routine()
+{
+  Serial.println("Rove_status: ascent");
+  
+  current_rover_state = descent;
+
+}
+
+
+void *descent_routine()
+{
+  Serial.println("Rove_status: descent");
+  
+  current_rover_state = navigation;
+
+}
+
+
+void *navigation_routine()
+{
+  Serial.println("Rove_status: navigation");
+  
+  current_rover_state = closing_up;
+
+}
+
+
+void *closing_up_routine()
+{
+  Serial.println("Rove_status: closing_up");
+  
+  current_rover_state = end_navigation;
+
+}
+
+
+
+void *end_navigation_routine()
+{
+  Serial.println("Rove_status: end_navigation");
+  
+  
+}
 
 
 
